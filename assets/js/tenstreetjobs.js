8c127edbc138e3168bd7e656604b217ca73bbcdf
Ryan Simpson <ryan.simpson@postalfleetsvs.com>
2020-12-16 14:22:40 -0500

    Tenstreet Datatable WIP

--- assets/js/tenstreetjobs.js
+++ assets/js/tenstreetjobs.js
@@ -11,30 +11,22 @@ function loadXMLDoc() {
 function myFunction(xml) {
   var i;
   var xmlDoc = xml.responseXML;
-  var table =
-    "<tr><th>Requisition ID</th><th>Job Title</th><th>Start Date</th><th>End Date</th><th>City</th></tr>";
+  var table = "<thead><tr><th>Requisition ID</th><th>Start Date</th><th>End Date</th></tr></thead><tbody>";
   var x = xmlDoc.getElementsByTagName("JobRequisition");
   for (i = 0; i < x.length; i++) {
     table +=
       "<tr><td>" +
       x[i].getElementsByTagName("RequisitionId")[0].childNodes[0].nodeValue +
       "</td><td>" +
-      (x[i].getElementsByTagName("JobTitle")[0].childNodes.length >= 1
-        ? x[i].getElementsByTagName("JobTitle")[0].childNodes[0].nodeValue
-        : "") +
-      "</td><td>" +
       x[i].getElementsByTagName("StartDate")[0].childNodes[0].nodeValue +
       "</td><td>" +
       x[i].getElementsByTagName("EndDate")[0].childNodes[0].nodeValue +
-      "</td><td>" +
-      x[i].getElementsByTagName("City")[0].childNodes[0].nodeValue +
-      "</td><td><a target='_blank' href=" +
-      (x[i].getElementsByTagName("URL")[0].childNodes.length >= 1
-        ? x[i].getElementsByTagName("URL")[0].childNodes[0].nodeValue
-        : "") +
-      ">" +
-      "Job Link" +
-      "</a></td></tr>";
+      "</td></tr>";
   }
-  document.getElementById("demo").innerHTML = table;
+  table += "</tbody>";
+
+  // console.log(table);
+  document.getElementById("example").innerHTML = table;
 }
+
+loadXMLDoc();
