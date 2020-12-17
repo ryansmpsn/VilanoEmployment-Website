275eb098a81a8d7f6dddce286fd450d8f1b9228f
Ryan Simpson <ryzizn@gmail.com>
2020-12-17 10:18:32 -0500

    XML Live Test

--- assets/js/tenstreetjobs.js
+++ assets/js/tenstreetjobs.js
@@ -5,28 +5,42 @@ function loadXMLDoc() {
       myFunction(this);
     }
   };
-  xmlhttp.open("GET", "tenstreet.xml", true);
+  xmlhttp.open(
+    "GET",
+    "https://dashboard.tenstreet.com/api/jr/job_requisitions.php?company_id=34969&jr_api_key=postaljobreqs&client_id=177&password=gh2lkb$P5$dz1Rj4$ko$^N",
+    true
+  );
   xmlhttp.send();
 }
 function myFunction(xml) {
   var i;
   var xmlDoc = xml.responseXML;
-  var table = "<thead><tr><th>Requisition ID</th><th>Start Date</th><th>End Date</th></tr></thead><tbody>";
+  var table =
+    "<thead><tr><th>Job Company</th><th>Job Title</th><th>Description</th><th>City</th><th>State</th><th>Job Link</th></tr></thead><tbody>";
   var x = xmlDoc.getElementsByTagName("JobRequisition");
   for (i = 0; i < x.length; i++) {
     table +=
       "<tr><td>" +
-      x[i].getElementsByTagName("RequisitionId")[0].childNodes[0].nodeValue +
+      x[i].getElementsByTagName("JobCompany")[0].childNodes[0].nodeValue +
       "</td><td>" +
-      x[i].getElementsByTagName("StartDate")[0].childNodes[0].nodeValue +
+      (x[i].getElementsByTagName("JobTitle")[0].childNodes.length >= 1
+        ? x[i].getElementsByTagName("JobTitle")[0].childNodes[0].nodeValue
+        : x[i].getElementsByTagName("Category")[0].childNodes[0].nodeValue) +
       "</td><td>" +
-      x[i].getElementsByTagName("EndDate")[0].childNodes[0].nodeValue +
-      "</td></tr>";
+      x[i].getElementsByTagName("JobDescription")[0].childNodes[0].nodeValue +
+      "</td><td>" +
+      x[i].getElementsByTagName("City")[0].childNodes[0].nodeValue +
+      "</td><td>" +
+      (x[i].getElementsByTagName("State")[0].childNodes.length >= 1
+        ? x[i].getElementsByTagName("State")[0].childNodes[0].nodeValue
+        : " ") +
+      "</td><td><a target='_blank' href=" +
+      x[i].getElementsByTagName("URL")[0].childNodes[0].nodeValue +
+      ">Apply</a></td></tr>";
   }
+  console.log(xmlDoc.getElementsByTagName("JobRequisition")[15]);
   table += "</tbody>";
-
-  // console.log(table);
-  document.getElementById("example").innerHTML = table;
+  document.getElementById("careerTable").innerHTML = table;
 }
 
 loadXMLDoc();
